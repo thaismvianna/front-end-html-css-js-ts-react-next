02962d735828deacc1028ee7517ba4c4251a9246
thaismvianna <thaismvianna@outlook.com>
2025-04-16 22:37:35 -0300

    primeiro teste js

---
+++ exercicios/09/script.js
@@ -0,0 +1,14 @@
+// alert("Hello, World!")
+
+var hora = new Date().getHours()
+var saudacao
+
+if(hora < 12) {
+    saudacao = "Bom dia"
+} else if(hora < 18) {
+    saudacao = "Boa tarde"
+} else {
+    saudacao = "Boa noite"
+}
+
+document.getElementById("mensagem").innerHTML = saudacao 
